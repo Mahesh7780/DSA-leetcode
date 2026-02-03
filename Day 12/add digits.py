7a6be4a22dbf5aaad6f224fc96984bd91b7f2ddc
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-03 23:01:18 +0530

    add digit sum
    Implement addDigits method to sum the digits of a number until a single digit is obtained.

---
+++ Day 12/add digits.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/add-digits/description/
+class Solution:
+    def addDigits(self, num: int) -> int:
+
+        # while num >= 10:
+        #     s = 0
+        #     t = num 
+        #     while t > 0:
+        #         s += t % 10
+        #         t //= 10
+        #         num= s
+        # return(num)
+# OR
+        while num >=10:
+    
+            b= [int(d) for d in str(num)]
+            num=sum(b)
+        return num
+        
