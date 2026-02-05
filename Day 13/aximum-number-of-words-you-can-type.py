0f674549be101b6acb700c0dd21f8c14d6dd22ce
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-05 22:10:28 +0530

    maximum number of typeable words
    Implement solution to count typeable words.

---
+++ Day 13/aximum-number-of-words-you-can-type.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/maximum-number-of-words-you-can-type/description/?envType=daily-question&envId=2026-02-05
+class Solution(object):
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        m = text.split()
+        count = 0
+        for i in m:
+            for j in brokenLetters:
+                if j in i:
+                    break
+            else:
+                count += 1
+        return count
+        
