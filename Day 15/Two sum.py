e8e2b35d1d5d2f2e7d22c7c2f18cbe2fcf61e7b3
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-07 22:45:21 +0530

    Rename file to 'Two sum.py'

---
+++ Day 15/Two sum.py
@@ -0,0 +1,14 @@
+
+#https://leetcode.com/problems/two-sum/  
+class Solution:
+    def twoSum(self, nums, target):
+
+        seen = {} 
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+
+        
