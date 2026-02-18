db97d4e612de647d14016351bd2954632f9fcc53
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-18 23:11:42 +0530

    Add distributeCandies function to solve candy problem
    Implement solution for distributing candies based on types.

---
+++ Day 19/distribute-candies.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/distribute-candies/description/
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        n=len(candyType)   #lenof candy
+        m=len(set(candyType)) # candy to set
+        a=len(candyType)//2 #len//2
+        if m>a: #set len > a
+            return a
+        return m
+
+
+        #or
+        return min(len(set(candyType)),len(candyType)//2)
+
+        
