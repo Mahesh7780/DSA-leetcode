f1bfa62b7da1c1a56691668c8108142a26bdbd12
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-22 22:47:09 +0530

    ...
    Removed commented-out code for clarity.

--- Day 12/add digits.py
+++ Day 12/add digits.py
@@ -1,7 +1,6 @@
 #https://leetcode.com/problems/add-digits/description/
 class Solution:
     def addDigits(self, num: int) -> int:
-
         # while num >= 10:
         #     s = 0
         #     t = num 
