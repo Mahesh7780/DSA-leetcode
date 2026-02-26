fdf68e75fec2eaab95af23fff99b668d0e6c569d
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-26 22:29:44 +0530

    Refactor addDigits method for digit summation
    Refactor addDigits method to use a while loop for summing digits.

--- Day 12/add digits.py
+++ Day 12/add digits.py
@@ -1,12 +1,12 @@
 #https://leetcode.com/problems/add-digits/description/
 class Solution:
     def addDigits(self, num: int) -> int:
-        # while num >= 10:
-        #     s = 0
-        #     t = num 
-        #     while t > 0:
-        #         s += t % 10
-        #         t //= 10
+         while num >= 10:
+             s = 0
+             t = num 
+             while t > 0:
+                 s += t % 10
+                 t //= 10
                  num= s
         return(num)
 # OR
