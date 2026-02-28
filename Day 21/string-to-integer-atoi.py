c38c5e8e1355e4d4399e19a079121b015abe9e27
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-02-28 22:55:09 +0530

    Implement string to integer conversion function

---
+++ Day 21/string-to-integer-atoi.py
@@ -0,0 +1,40 @@
+#https://leetcode.com/problems/string-to-integer-atoi/
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip() 
+        if not s:
+            return 0
+        sign = 1
+        i = 0
+        if s[0] == "-":
+            sign = -1
+            i += 1
+        elif s[0] == "+":
+            i += 1
+
+        num = 0
+        INT_MAX = 2**31 - 1
+        INT_MIN = -(2**31)
+
+        while i < len(s) and s[i].isdigit():
+            digit = int(s[i])
+            if num > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+
+            num = num * 10 + digit
+            i += 1
+
+        return sign * num
+
+#class Solution:
+#     def myAtoi(self, n: str) -> int:
+#         c=n.replace(" ", "") and n.replace("0","")
+#         ch="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ +-."
+#         temp=""
+#         for c in n:
+#             if not n or not n[0].isdigit():
+#                 print(0)
+#             if c in ch:
+#                 break
+#                 temp+=c
+#         return(temp)
