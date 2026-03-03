bfb58b46d33da03ff71569d370fa7ddaf709f6f7
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-03-03 23:36:16 +0530

    Create multiply-strings.py

---
+++ Day 24/multiply-strings.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/multiply-strings/
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        a=int(num1)
+        b=int(num2)
+        c=a*b
+        return str(c)
