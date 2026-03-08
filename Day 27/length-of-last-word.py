68d582e2e7f3a063792329f6d26c5552de8eddbb
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-03-08 22:12:16 +0530

    Create length-of-last-word.py

---
+++ Day 27/length-of-last-word.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/length-of-last-word/description/
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        m=s.split() # i split the sentence
+        l=len(m[-1]) # storing the length of last word in the sentence
+        return l
+        
