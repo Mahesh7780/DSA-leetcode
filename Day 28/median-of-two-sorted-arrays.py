53d9e029dfdb941f661909e604a0eb5fd5469044
Mahesh <144904051+Mahesh7780@users.noreply.github.com>
2026-03-13 23:07:58 +0530

    Create median-of-two-sorted-arrays.py

---
+++ Day 28/median-of-two-sorted-arrays.py
@@ -0,0 +1,18 @@
+# import numpy as np
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
+        d = nums1 + nums2
+        c = sorted(d)
+        n = len(c)
+
+        if n % 2 == 1:
+            return c[n // 2]
+        else:
+            return (c[n // 2 - 1] + c[n // 2]) / 2.0
+
+        # c=nums1+nums2
+        # d=sorted(c)
+        # m=np.median(d)
+        # return m
